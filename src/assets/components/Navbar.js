1f3972acdecfafa97cd4f2a6185b992f23877112
sorin <sorinmartinescu06@gmail.com>
2022-08-17 20:39:52 +0200

    added fonts

---
+++ src/assets/components/Navbar.js
@@ -0,0 +1,20 @@
+import '../css/navbar.css'
+
+export default function Navbar() {
+    return(
+        <>
+         <div id='container'>
+            <a id='logo'>Logo</a>
+            <div id='nav-links'>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Projects</a>
+                <a href="#">Contact</a>
+            </div>
+            <div id='ham-menu'>
+                menu
+            </div>
+         </div>
+        </>
+    )
+}
