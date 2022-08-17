4905f1568c94488151cbedfe65fd8370be638370
sorin <sorinmartinescu06@gmail.com>
2022-08-18 01:38:53 +0200

    added footer

--- src/assets/components/Footer.js
+++ src/assets/components/Footer.js
@@ -1,8 +1,20 @@
+import instagram from '../images/socials/instagram.png'
+import github from '../images/socials/github.png'
+import dev from '../images/socials/dev.png'
 import '../css/footer.css'
-
 export default function Footer() {
-    return(
-        <>
-        </>
-    )
+  return (
+    <>
+    <div className='test'>
+    <div id="footer-container">
+        <a id='footer-text'>Made by <span><a href='https://github.com/SorinXV'>SorinXV</a></span>, 2022</a>
+        <div id="socialmedia-icons">
+          <a href='https://www.instagram.com/sorinxv/'><img src={instagram} alt="instagram socialmedia icon" /></a>
+          <a href="https://github.com/SorinXV"></a><img src={github} alt="github icon" />
+          <a href="https://dev.to/sorinxv"><img src={dev} alt="dev socialmedia icon" /></a>
+        </div>
+      </div>
+    </div>
+    </>
+  )
 }
