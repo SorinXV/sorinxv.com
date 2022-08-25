140bb0fcc7167210998272a520ed1fd3612c68e5
sorin <sorinmartinescu06@gmail.com>
2022-08-25 05:17:00 +0200

    cleaned project file

--- src/assets/pages/Projects.js
+++ src/assets/pages/Projects.js
@@ -1,9 +1,9 @@
 import '../css/projects.css'
-
+import project from '../jsonfiles/projects.json'
 export default function Projects() {
-    return(
-        <>
-        Projects
-        </>
-    )
+ return(
+  <>
+  
+  </>
+ )
 }
