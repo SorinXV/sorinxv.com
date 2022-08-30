266848b042f77c89ac5c36b929d58a8037356d43
sorin <sorinmartinescu06@gmail.com>
2022-08-30 23:02:09 +0200

    added another container which will be used for img

--- src/assets/pages/Home.js
+++ src/assets/pages/Home.js
@@ -1,19 +1,19 @@
 import '../css/home.css'
 import home from '../jsonfiles/home.json'
 export default function Home() {
-    return(
-        <>
-            {
-                home.map( home => {
-                    return(
-                        <div id='container'>
-                            <a id='second-title'>{home.secondTitle}</a>
-                            <a id='title'>{home.title}</a>
-                            <a id='paragraph'>{home.paragraph}</a>
-                        </div>
-                    )
-                })
-            }
-        </>
-    )
+  return (
+    <>
+      {home.map((home) => {
+        return (
+          <div>
+            <div id="container">
+              <a id="second-title">{home.secondTitle}</a>
+              <a id="title">{home.title}</a>
+              <a id="paragraph">{home.paragraph}</a>
+            </div>
+          </div>
+        )
+      })}
+    </>
+  )
 }
