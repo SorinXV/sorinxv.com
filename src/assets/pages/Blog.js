6b45f84699fb60d1947a8b2d6c6310ba8e851ab6
sorin <sorinmartinescu06@gmail.com>
2022-09-11 21:36:52 +0200

    added blog jsx page

--- src/assets/pages/Blog.js
+++ src/assets/pages/Blog.js
@@ -1,9 +1,21 @@
 import '../css/blog.css'
-
+import '../css/text.css'
+import blog from '../jsonfiles/blog.json'
 export default function Blog() {
-    return(
-        <>
-        blog
-        </>
-    )
+  return (
+    <>
+      {blog.map((blog) => {
+        return (
+          <div className='container'>
+            
+            <a className='small-title ' id='margin-top'>{blog.articleName}</a>
+            <a className='date'>{blog.articleDate}</a>
+            <a className='description'>{blog.description}</a>
+            <a className='date' id='color'>{blog.tech}</a>
+            
+          </div>
+        )
+      })}
+    </>
+  )
 }
