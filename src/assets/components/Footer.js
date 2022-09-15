5d705c0ae60e6c4cf3212d5dd553a0b337afe3a1
sorin <sorinmartinescu06@gmail.com>
2022-09-16 00:58:36 +0200

    footer icons changed

--- src/assets/components/Footer.js
+++ src/assets/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import instagram from '../images/socials/instagram.png'
-import github from '../images/socials/github.png'
-import dev from '../images/socials/dev.png'
+import github from '../images/socials/github.svg'
+import linkedin from '../images/socials/linkedin.svg'
+import instagram from '../images/socials/instagram.svg'
 import '../css/footer.css'
 export default function Footer() {
   return (
@@ -10,9 +10,9 @@ export default function Footer() {
     <div id="footer-container">
         <a id='footer-text'>Made by <span><a href='https://github.com/SorinXV'>SorinXV</a></span>, 2022</a>
         <div id="socialmedia-icons">
-          <a href='https://www.instagram.com/sorinxv/'><img src={instagram} className="icon" alt="instagram socialmedia icon" /></a>
-          <a href="https://github.com/SorinXV"></a><img src={github} className="icon" alt="github icon" />
-          <a href="https://dev.to/sorinxv"><img src={dev} className="icon" alt="dev socialmedia icon" /></a>
+        <a href="https://github.com/SorinXV"></a><img src={github} className="icon" alt="github icon" />
+        <a href='https://www.instagram.com/sorinxv/'><img src={instagram} className="icon" alt="instagram socialmedia icon" /></a>
+        <a href='#'><img src={linkedin} className="linkedin" alt="linkedin socialmedia icon" /></a>     
         </div>
       </div>
     </div>
