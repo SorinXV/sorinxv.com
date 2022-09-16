28f699e2aa90db54ef91936eeb98b577c29fe788
sorin <sorinmartinescu06@gmail.com>
2022-09-16 16:11:10 +0200

    contact page

--- src/assets/pages/Contact.js
+++ src/assets/pages/Contact.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import '../css/text.css'
 import '../css/contact.css'
 
 export default function Contact() {
-    return(
-        <>
-        contact
-        </>
-    )
+  return (
+    <>
+      <div id='mail-container' >
+    
+        <p className='paragraph' id='mb'>
+        I’m currently open for any new opportunities, I’ll try my best to get back to you asap!
+        </p>
+        <h2> Contact</h2>
+       
+        <input type="text" name="firstname" placeholder="Name" />
+        <input type="text" name="youremail" placeholder="Email" />
+        <input type="text" name="Message" placeholder="Message" id="message" />
+        <input type="submit" value="Submit" id='submit'/>
+      </div>
+      
+    </>
+  )
 }
