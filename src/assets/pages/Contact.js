6456da518a071ddfea61ca03e0be520d391e2fdc
sorin <sorinmartinescu06@gmail.com>
2022-09-16 20:48:37 +0200

    responsive contact page, not functional yet

--- src/assets/pages/Contact.js
+++ src/assets/pages/Contact.js
@@ -5,19 +5,24 @@ import '../css/contact.css'
 export default function Contact() {
   return (
     <>
-      <div id='mail-container' >
-    
-        <p className='paragraph' id='mb'>
-        I’m currently open for any new opportunities, I’ll try my best to get back to you asap!
+      <div id="mail-container">
+        <p className="paragraph" id="mb">
+          I’m currently open to any new opportunities, I’ll try my best to get
+          back to you asap!<br/>
+          You can get in touch with me by sending an email at
+          contact@sorinxv.com or message me on instagram
         </p>
         <h2> Contact</h2>
-       
         <input type="text" name="firstname" placeholder="Name" />
         <input type="text" name="youremail" placeholder="Email" />
-        <input type="text" name="Message" placeholder="Message" id="message" />
-        <input type="submit" value="Submit" id='submit'/>
+        <textarea
+          type="text"
+          name="Message"
+          placeholder="Message"
+          id="message"
+        />
+        <input type="submit" value="Submit" id="submit" />
       </div>
-      
     </>
   )
 }
