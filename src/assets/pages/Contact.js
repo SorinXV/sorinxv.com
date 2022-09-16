a043dd5e7d61002d2dd7c3175fb7fe0aa719c798
sorin <sorinmartinescu06@gmail.com>
2022-09-16 22:01:16 +0200

    email system now works

--- src/assets/pages/Contact.js
+++ src/assets/pages/Contact.js
@@ -1,27 +1,42 @@
 import React from 'react'
 import '../css/text.css'
 import '../css/contact.css'
-
+import emailjs from 'emailjs-com'
 export default function Contact() {
+  function sendEmail(e) {
+    e.preventDefault()
+
+    emailjs.sendForm(
+      'service_iwdtbcy',
+      'template_mew9h9r',
+      e.target,
+      'yH5zSHqkbFs3LlDQI',
+    ).then(res=>{
+        console.log(res);
+    }).catch(err=> console.log(err));
+  }
   return (
     <>
       <div id="mail-container">
         <p className="paragraph" id="mb">
           I’m currently open to any new opportunities, I’ll try my best to get
-          back to you asap!<br/>
+          back to you asap!
+          <br />
           You can get in touch with me by sending an email at
           contact@sorinxv.com or message me on instagram
         </p>
         <h2> Contact</h2>
-        <input type="text" name="firstname" placeholder="Name" />
-        <input type="text" name="youremail" placeholder="Email" />
-        <textarea
-          type="text"
-          name="Message"
-          placeholder="Message"
-          id="message"
-        />
-        <input type="submit" value="Submit" id="submit" />
+        <form onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Name" />
+          <input type="text" name="user_email" placeholder="Email" />
+          <textarea
+            type="text"
+            name="message"
+            placeholder="Message"
+            id="message"
+          />
+          <input type="submit" value="send" id="submit" />
+        </form>
       </div>
     </>
   )
