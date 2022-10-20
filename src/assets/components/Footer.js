904eaf0f99f1620b9a0d2d464dcbab7b36e10900
sorinxv <sorinmartinescu06@gmail.com>
2022-10-20 22:15:55 +0200

    added linkedin

--- src/assets/components/Footer.js
+++ src/assets/components/Footer.js
@@ -8,11 +8,11 @@ export default function Footer() {
     <>
     <div className='test'>
     <div id="footer-container">
-        <a id='footer-text'>Made by <span><a href='https://github.com/SorinXV'>SorinXV</a></span>, 2022</a>
+        <a href='#' id='footer-text'>Made by <span><a href='https://github.com/SorinXV'>SorinXV</a></span>, 2022</a>
         <div id="socialmedia-icons">
-        <a href="https://github.com/SorinXV"><img src={github} className="icon" alt="github icon" /></a>
-        <a href='https://www.instagram.com/sorinxv/'><img src={instagram} className="icon" alt="instagram socialmedia icon" /></a>
-        <a href='#'><img src={linkedin} className="linkedin icon" alt="linkedin socialmedia icon" /></a>     
+        <a href="https://github.com/SorinXV" target={'_blank'}><img src={github} className="icon" alt="github icon" /></a>
+        <a href='https://www.instagram.com/sorinxv/'target={'_blank'}><img src={instagram} className="icon" alt="instagram socialmedia icon" /></a>
+        <a  href='https://www.linkedin.com/in/sorin-martinescu-07149a216/' target={'_blank'}><img src={linkedin} className="linkedin icon" alt="linkedin socialmedia icon" /></a>     
         </div>
       </div>
     </div>
