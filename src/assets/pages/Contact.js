382d7c01fe6aa6f375c979770eb5d22dcfce82a9
sorinxv <sorinmartinescu06@gmail.com>
2022-10-20 22:16:42 +0200

    re-formuled the sentence

--- src/assets/pages/Contact.js
+++ src/assets/pages/Contact.js
@@ -11,19 +11,19 @@ export default function Contact() {
       'template_mew9h9r',
       e.target,
       'yH5zSHqkbFs3LlDQI',
-    ).then(res=>{
-        console.log(res);
-    }).catch(err=> console.log(err));
+    ).then(res => {
+      console.log(res);
+    }).catch(err => console.log(err));
   }
   return (
     <>
       <div id="mail-container">
         <p className="paragraph" id="mb">
+
+          You can get in touch with me by sending an email at
+          contact@sorinxv.com or message me on instagram 
           I’m currently open to any new opportunities, I’ll try my best to get
           back to you asap!
-          <br />
-          You can get in touch with me by sending an email at
-          contact@sorinxv.com or message me on instagram
         </p>
         <h2> Contact</h2>
         <form onSubmit={sendEmail}>
